a59333083435565c495aead9209739a5cf1b1d99
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-11-16 15:54:58 +0330

    add controller

---
+++ CA1/trunk/src/hdl/controller.v
@@ -0,0 +1,47 @@
+`timescale 1ps/
+`define IDLE   3'b000
+`define INIT   3'b001
+`define MULT   3'b010
+`define ADD    3'b011
+`define WB_ACT 3'b100
+`define CHECK  3'b101
+`define DONE   3'110
+module controller(start, rst, clk, isfinished, init_w, init_x, load_a, load_sel, done);
+    input start,clk,rst,isfinished;
+    output init_w, init_x, load_a, load_sel, done;
+    reg[2:0] ns,ps;
+
+    always @(start, isfinished) begin 
+            `IDLE:   ns <= ~start ? IDLE : `INIT;
+            `INIT:   ns <= start ? `INIt ? MULT;
+            `MULT:   ns <= `ADD;
+            `ADD:    ns <= `WB_ACT;
+            `WB_ACT: ns <= `CHECK;
+            `CHEKC:  ns <= isfinished ? `DONE : MULT;
+            `DONE :  ns <= `IDLE; 
+    end
+
+    always @(ps) begin
+        {init_w, init_x, load_a, load_sel, done} = 5'b0;
+        case(ps)
+            `IDLE:begin end
+            `INIT:begin
+                {init_w, init_x, load_a, load_sel} = 4'b0; end
+            `MULT: begin end
+            `ADD: begin end
+            `WB_ACT: begin load_a = 1'b1; end
+            `CHEKC: begin end
+            `DONE : begin  done = 1'b1; end
+        endcase
+    end
+
+    always @(posedge clk, posedge rst) begin
+        if (rst)
+            ps <= `IDLE;
+        else
+         ps <= ns;
+     end
+
+
+
+endmodule
--- CA1/trunk/src/hdl/register.v
+++ CA1/trunk/src/hdl/register.v
@@ -4,9 +4,9 @@ module register(parameter N = 32)(clk, ld, in , out);
     input[N-1:0] in;
     output[N-1:0] out;
 
-    always @(clk){
+    always @(posedge clk)begin 
         if(ld) 
             out = in;
-    }
+    end
 
 endmodule 
