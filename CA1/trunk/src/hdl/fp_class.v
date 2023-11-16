e8112f2990118d74c0ca636001b3ed60700d8b32
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-11-16 12:54:29 +0330

    Add floating point operations

---
+++ CA1/trunk/src/hdl/fp_class.v
@@ -0,0 +1,57 @@
+`timescale 1ps / 1ps
+
+module fp_class(f, fExp, fSig, fFlags);
+    parameter NEXP = 5;
+    parameter NSIG = 10;
+    parameter CLOG2_NSIG = $clog2(NSIG+1);
+    input [NEXP+NSIG:0] f;
+    output signed [NEXP+1:0] fExp;
+    reg signed [NEXP+1:0] fExp;
+    output [NSIG:0] fSig;
+    reg [NSIG:0] fSig;
+    `include "ieee-754-flags.v"
+    output [LAST_FLAG-1:0] fFlags;
+
+    wire expOnes, expZeroes, sigZeroes;
+
+    assign expOnes   =  &f[NEXP+NSIG-1:NSIG];
+    assign expZeroes = ~|f[NEXP+NSIG-1:NSIG];
+    assign sigZeroes = ~|f[NSIG-1:0];
+
+    assign fFlags[SNAN]      =  expOnes   & ~sigZeroes & ~f[NSIG-1];
+    assign fFlags[QNAN]      =  expOnes                &  f[NSIG-1];
+    assign fFlags[INFINITY]  =  expOnes   &  sigZeroes;
+    assign fFlags[ZERO]      =  expZeroes &  sigZeroes;
+    assign fFlags[SUBNORMAL] =  expZeroes & ~sigZeroes;
+    assign fFlags[NORMAL]    = ~expOnes   & ~expZeroes;
+
+    reg [NSIG:0] mask = ~0;
+    reg [CLOG2_NSIG-1:0] sa;
+
+    integer i;
+
+    always @(*)
+      begin
+        fExp = f[NEXP+NSIG-1:NSIG];
+        fSig = f[NSIG-1:0];
+
+        sa = 0;
+
+        if (fFlags[NORMAL])
+          {fExp, fSig} = {f[NEXP+NSIG-1:NSIG] - BIAS, 1'b1, f[NSIG-1:0]};
+        else if (fFlags[SUBNORMAL])
+          begin
+            for (i = (1 << (CLOG2_NSIG - 1)); i > 0; i = i >> 1)
+              begin
+                if ((fSig & (mask << (NSIG + 1 - i))) == 0)
+                  begin
+                    fSig = fSig << i;
+                    sa = sa | i;
+                  end
+              end
+
+            fExp = EMIN - sa;
+          end
+      end
+
+endmodule
---
+++ CA1/trunk/src/hdl/fp_multiplier.v
@@ -0,0 +1,110 @@
+`timescale 1ps / 1ps
+
+module fp_mul(a, b, p, pFlags);
+  parameter NEXP = 5;
+  parameter NSIG = 10;
+  input [NEXP+NSIG:0] a, b;
+  output [NEXP+NSIG:0] p;
+  `include "ieee-754-flags.v"
+  output [LAST_FLAG-1:0] pFlags;
+  reg [LAST_FLAG-1:0] pFlags;
+
+  wire signed [NEXP+1:0] aExp, bExp;
+  reg signed [NEXP+1:0] pExp, t1Exp, t2Exp;
+  wire [NSIG:0] aSig, bSig;
+  reg [NSIG:0] pSig, tSig;
+
+  reg [NEXP+NSIG:0] pTmp;
+
+  wire [2*NSIG+1:0] rawSignificand;
+
+  wire [LAST_FLAG-1:0] aFlags, bFlags;
+
+  reg pSign;
+
+  fp_class #(NEXP,NSIG) aClass(a, aExp, aSig, aFlags);
+  fp_class #(NEXP,NSIG) bClass(b, bExp, bSig, bFlags);
+
+  assign rawSignificand = aSig * bSig;
+
+  always @(*)
+  begin
+    pSign = a[NEXP+NSIG] ^ b[NEXP+NSIG];
+    pTmp = {pSign, {NEXP{1'b1}}, 1'b0, {NSIG-1{1'b1}}};  // Initialize p to be an sNaN.
+    pFlags = 6'b000000;
+
+    if ((aFlags[SNAN] | bFlags[SNAN]) == 1'b1)
+      begin
+        pTmp = aFlags[SNAN] == 1'b1 ? a : b;
+        pFlags[SNAN] = 1;
+      end
+    else if ((aFlags[QNAN] | bFlags[QNAN]) == 1'b1)
+      begin
+        pTmp = aFlags[QNAN] == 1'b1 ? a : b;
+        pFlags[QNAN] = 1;
+      end
+    else if ((aFlags[INFINITY] | bFlags[INFINITY]) == 1'b1)
+      begin
+        if ((aFlags[ZERO] | bFlags[ZERO]) == 1'b1)
+          begin
+            pTmp = {pSign, {NEXP{1'b1}}, 1'b1, {NSIG-1{1'b0}}}; // qNaN
+            pFlags[QNAN] = 1;
+          end
+        else
+          begin
+            pTmp = {pSign, {NEXP{1'b1}}, {NSIG{1'b0}}};
+            pFlags[INFINITY] = 1;
+          end
+      end
+    else if ((aFlags[ZERO] | bFlags[ZERO]) == 1'b1 ||
+             (aFlags[SUBNORMAL] & bFlags[SUBNORMAL]) == 1'b1)
+      begin
+        pTmp = {pSign, {NEXP+NSIG{1'b0}}};
+        pFlags[ZERO] = 1;
+      end
+    else // if (((aFlags[SUBNORMAL] | aFlags[NORMAL]) & (bFlags[SUBNORMAL] | bFlags[NORMAL])) == 1'b1)
+      begin
+        t1Exp = aExp + bExp;
+
+        if (rawSignificand[2*NSIG+1] == 1'b1)
+          begin
+            tSig = rawSignificand[2*NSIG+1:NSIG+1];
+            t2Exp = t1Exp + 1;
+          end
+        else
+          begin
+            tSig = rawSignificand[2*NSIG:NSIG];
+            t2Exp = t1Exp;
+          end
+
+        if (t2Exp < (EMIN - NSIG))  // Too small to even be represented as
+          begin                     // a subnormal; round down to zero.
+            pTmp = {pSign, {NEXP+NSIG{1'b0}}};
+            pFlags[ZERO] = 1;
+          end
+        else if (t2Exp < EMIN) // Subnormal
+          begin
+            pSig = tSig >> (EMIN - t2Exp);
+            // Remember that we can only store NSIG bits
+            pTmp = {pSign, {NEXP{1'b0}}, pSig[NSIG-1:0]};
+            pFlags[SUBNORMAL] = 1;
+          end
+        else if (t2Exp > EMAX) // Infinity
+          begin
+            pTmp = {pSign, {NEXP{1'b1}}, {NSIG{1'b0}}};
+            pFlags[INFINITY] = 1;
+          end
+        else // Normal
+          begin
+            pExp = t2Exp + BIAS;
+            pSig = tSig;
+
+            pTmp = {pSign, pExp[NEXP-1:0], pSig[NSIG-1:0]};
+	    pFlags[NORMAL] = 1;
+          end
+      end //
+  end
+
+  assign p = pTmp;
+
+endmodule
