8904f24894c55434ad2e3cffbb3921dea3a7e167
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-11-17 11:56:16 +0330

    Refactor multiplier

--- CA1/trunk/src/hdl/fp_multiplier.v
+++ CA1/trunk/src/hdl/fp_multiplier.v
@@ -1,36 +1,31 @@
 `timescale 1ps / 1ps
 
-module FloatingMultiplication #(parameter XLEN=32)
-                                (input [XLEN-1:0]A,
-                                 input [XLEN-1:0]B,
-                                 input clk,
-                                 output overflow,
-                                 output underflow,
-                                 output exception,
-                                 output reg  [XLEN-1:0] result);
+module fp_mult #(parameter N=32) (a, b, result);
 
-reg [23:0] A_Mantissa,B_Mantissa;
-reg [22:0] Mantissa;
-reg [47:0] Temp_Mantissa;
-reg [7:0] A_Exponent,B_Exponent,Temp_Exponent,diff_Exponent,Exponent;
-reg A_sign,B_sign,Sign;
-reg [32:0] Temp;
-reg [6:0] exp_adjust;
-always@(*)
-begin
-A_Mantissa = {1'b1,A[22:0]};
-A_Exponent = A[30:23];
-A_sign = A[31];
-  
-B_Mantissa = {1'b1,B[22:0]};
-B_Exponent = B[30:23];
-B_sign = B[31];
+    input [N-1:0]a;
+    input [N-1:0]b;
+    output reg [N-1:0] result;
 
-Temp_Exponent = A_Exponent+B_Exponent-127;
-Temp_Mantissa = A_Mantissa*B_Mantissa;
-Mantissa = Temp_Mantissa[47] ? Temp_Mantissa[46:24] : Temp_Mantissa[45:23];
-Exponent = Temp_Mantissa[47] ? Temp_Exponent+1'b1 : Temp_Exponent;
-Sign = A_sign^B_sign;
-result = {Sign,Exponent,Mantissa};
-end
+    reg [23:0] a_mantis, b_mantis;
+    reg [22:0] res_mantis;
+    reg [47:0] temp_mantis;
+    reg [7:0] a_exp, b_exp, temp_exp, res_exp;
+    reg a_sign, b_sign, res_sign;
+    always@(*)
+    begin
+        a_mantis = {1'b1,a[22:0]};
+        a_exp = a[30:23];
+        a_sign = a[31];
+        
+        b_mantis = {1'b1,b[22:0]};
+        b_exp  = b[30:23];
+        b_sign  = b[31];
+
+        temp_exp = a_exp+b_exp- 127;
+        temp_mantis = a_mantis*b_mantis;
+        res_mantis = temp_mantis[47] ? temp_mantis[46:24] : temp_mantis[45:23];
+        res_exp = temp_mantis[47] ? temp_exp+1'b1 : temp_exp;
+        res_sign = a_sign^b_sign; 
+        result = {res_sign,res_exp,res_mantis};
+    end
 endmodule
