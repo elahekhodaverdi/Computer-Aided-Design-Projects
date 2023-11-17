4300b1c8d64d814ae278894b2443d9a9564fd214
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-11-17 13:04:23 +0330

    .

--- CA1/trunk/src/hdl/PU.v
+++ CA1/trunk/src/hdl/PU.v
@@ -10,19 +10,20 @@ module PU (clk, a1, a2, a3, a4, w, out);
     wire [31:0] addr1_res, addr2_res, addr3_res;
     wire [31:0] reg_addr_out;
 
-    FloatingMultiplication multiplier1(.clk(clk), .A(a1), .B(w[0]), .p(reg_mul1_inp));
-    FloatingMultiplication multiplier2(.clk(clk), .A(a2), .B(w[1]), .p(reg_mul2_inp));
-    FloatingMultiplication multiplier3(.clk(clk), .A(a3), .B(w[2]), .p(reg_mul3_inp));
-    FloatingMultiplication multiplier4(.clk(clk), .A(a4), .B(w[3]), .p(reg_mul4_inp));
+    fp_mult multiplier1(.a(a1), .b(w[0]), .result(reg_mul1_inp));
+    fp_mult multiplier1(.a(a2), .b(w[1]), .result(reg_mul2_inp));
+    fp_mult multiplier1(.a(a3), .b(w[2]), .result(reg_mul3_inp));
+    fp_mult multiplier1(.a(a4), .b(w[3]), .result(reg_mul4_inp));
 
+    
     register reg_mul1(clk, 1, reg_mul1_inp, reg_mul1_out);
     register reg_mul2(clk, 1, reg_mul2_inp, reg_mul2_out);
     register reg_mul3(clk, 1, reg_mul3_inp, reg_mul3_out);
     register reg_mul4(clk, 1, reg_mul4_inp, reg_mul4_out);
 
-    FloatingAddition addr1(.clk(clk), .A(reg_mul1_out), .B(reg_mul2_out), .result(addr1_res));
-    FloatingAddition addr2(.clk(clk), .A(reg_mul3_out), .B(reg_mul4_out), .result(addr2_res));
-    FloatingAddition addr3(.clk(clk), .A(addr1_res), .B(addr2_res), .result(addr3_res));
+    fp_adder addr1(.a(reg_mul1_out), .b(reg_mul2_out), .result(addr1_res));
+    fp_adder addr2(.a(reg_mul3_out), .b(reg_mul4_out), .result(addr2_res));
+    fp_adder addr1(.a(addr1_res), .b(addr2_res), .result(addr3_res));
 
     register reg_addr(clk, 1, addr3_res, reg_addr_out);
     activation act(reg_addr_out, out);
