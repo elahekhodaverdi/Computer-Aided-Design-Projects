670042dc0577bd5b2b1ccff41796d27479f8e3fc
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-12-13 13:03:07 +0330

    fix fulladder

--- CA2/trunk/src/hdl/fulladder.v
+++ CA2/trunk/src/hdl/fulladder.v
@@ -2,6 +2,5 @@ module fa(input a, b, cin, output sum, co);
     wire s;
     XOR x1(.a(a), .b(b), .out(s));
     XOR x1(.a(s), .b(cin), .out(sum));
-    carry_out carry (a,b,cin , co);
     carryOut carry(.a(a), .b(b), .c(cin), .cout(co));
 endmodule
