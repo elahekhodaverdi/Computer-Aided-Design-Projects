25e2d8d888a86fa15a5896d8525c6772b830c97d
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-12-13 11:19:40 +0330

    Add and4

---
+++ CA1/trunk/src/hdl/and4.v
@@ -0,0 +1,6 @@
+module AND4 (a, b, c, d, out);
+    input a, b, c, d;
+    output out;
+    C2 c2(.D0(1'b0), .D1(1'b0), .D2(1'b0), .D3(d), .A1(a), .B1(1'b0), .A0(b), .B0(c), .F(out));
+    
+endmodule
