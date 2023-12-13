69898d046244fbc38597687fbd2b995b8fc97a1e
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-12-13 16:17:43 +0330

    Add ld to register

--- CA2/trunk/src/hdl/register.v
+++ CA2/trunk/src/hdl/register.v
@@ -1,7 +1,7 @@
-module register (clk, rst, data_in, data_out);
+module register (clk, ld, data_in, data_out);
     parameter N = 32;
     input clk, rst;
     input [31 : 0] data_in;
     output [31 : 0] data_out;
-    S2 s2(.D0(data_in), .D1(0), .D2(0), .D3(0), .A1(0), .B1(0), .A0(0), .B0(0), .clr(rst), .clk(clk), .out(data_out));
+    S2 s2(.D0(data_out), .D1(0), .D2(data_in), .D3(0), .A1(0), .B1(0), .A0(0), .B0(0), .clr(0), .clk(clk), .out(data_out));
 endmodule
