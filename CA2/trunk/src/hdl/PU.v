8fd4d46f8712a6858589450b622e4c0c2c532957
Ali Hamzehpour <alihamzehpour2002@gmail.com>
2023-12-13 14:59:21 +0330

    Modify PU

--- CA2/trunk/src/hdl/PU.v
+++ CA2/trunk/src/hdl/PU.v
@@ -16,17 +16,17 @@ module PU (clk, a1, a2, a3, a4, w1, w2, w3, w4, out);
     multiplier multiplier3(.a(a3), .b(w3), .result(reg_mul3_inp));
     multiplier multiplier4(.a(a4), .b(w4), .result(reg_mul4_inp));
     
-    register reg_mul1(clk, 1'b1, reg_mul1_inp, reg_mul1_out);
-    register reg_mul2(clk, 1'b1, reg_mul2_inp, reg_mul2_out);
-    register reg_mul3(clk, 1'b1, reg_mul3_inp, reg_mul3_out);
-    register reg_mul4(clk, 1'b1, reg_mul4_inp, reg_mul4_out);
+    register reg_mul1(clk, 1'b0, reg_mul1_inp, reg_mul1_out);
+    register reg_mul2(clk, 1'b0, reg_mul2_inp, reg_mul2_out);
+    register reg_mul3(clk, 1'b0, reg_mul3_inp, reg_mul3_out);
+    register reg_mul4(clk, 1'b0, reg_mul4_inp, reg_mul4_out);
 
     // second cycle
     fulladder addr1(.a(reg_mul1_out), .b(reg_mul2_out), .result(addr1_res));
     fulladder addr2(.a(reg_mul3_out), .b(reg_mul4_out), .result(addr2_res));
     fulladder addr3(.a(addr1_res), .b(addr2_res), .result(addr3_res));
 
-    register reg_addr(clk, 1'b1, addr3_res, reg_addr_out);
+    register reg_addr(clk, 1'b0, addr3_res, reg_addr_out);
 
     // third cycle
     activation act(reg_addr_out, out);
