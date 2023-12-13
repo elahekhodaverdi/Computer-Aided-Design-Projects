3665a8769fda3a60a0744184df7b570b95ac7159
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-12-13 10:54:15 +0330

    initial CA2

---
+++ CA2/trunk/sim/sim_top.tcl
@@ -0,0 +1,55 @@
+	alias clc ".main clear"
+	
+	clc
+	exec vlib work
+	vmap work work
+	
+	set TB					"tb"
+	set hdl_path			"../src/hdl"
+	set inc_path			"../src/inc"
+	
+	set run_time			"100 us"
+#	set run_time			"-all"
+
+#============================ Add verilog files  ===============================
+# Pleas add other module here	
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/activation.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/check.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/check.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/controller.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/datapath.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/encoder4to2.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/fp_adder.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/fp_multiplier.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/memory.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/mux2-1.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/mux4-1.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/PU.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/register.v
+	vlog 	+acc -incr -source  +define+SIM 	$hdl_path/maxnet.v
+
+
+	vlog 	+acc -incr -source  +incdir+$inc_path +define+SIM 	./tb/$TB.v
+	onerror {break}
+
+#================================ simulation ====================================
+
+	vsim	-voptargs=+acc -debugDB $TB
+
+
+#======================= adding signals to wave window ==========================
+
+
+	add wave -hex -group 	 	{TB}				sim:/$TB/*
+	add wave -hex -group 	 	{top}				sim:/$TB/uut/*	
+	add wave -hex -group -r		{all}				sim:/$TB/*
+
+#=========================== Configure wave signals =============================
+	
+	configure wave -signalnamewidth 2
+    
+
+#====================================== run =====================================
+
+	run $run_time 
+	
