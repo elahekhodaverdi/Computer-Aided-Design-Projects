98e0f7e2d8e45a3d9acae9cd74b0bd3abad38bb9
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2023-12-15 12:18:25 +0330

    add abosule value module

--- CA2/trunk/sim/tb/adderTB.v
+++ CA2/trunk/sim/tb/adderTB.v
@@ -3,19 +3,17 @@ module adderTB();
     reg signed [9:0] a, b;
     reg cin;
     wire [10:0] sum;
-    wire co;
 
-    adder #(10) U1 (.a(a), .b(b), .sum(sum));
+    adder #(11) U1 (.a({a[9], a}), .b({b[9], b}), .sum(sum));
 
     initial begin
 
 
-        a = 32;
-        b = -24;
+        a = -32;
+        b = 24;
         cin = 1'b0;
 
         #10;
-
 
 
         $finish;
