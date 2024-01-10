4950ddcdae28a09e8ae71eaeed6a38772aba8d7d
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-01-10 15:45:02 +0330

    delete counters

--- CA4/trunk/src/hdl/controller.sv
+++ CA4/trunk/src/hdl/controller.sv
@@ -13,57 +13,80 @@
 `define DONE 4'b1001
 `define UPDATE 4'b1010
 
-module controller(clk, start, co_cntr16_img, co_cntr4_filter, co_cntr_filters,
-                 co_cntr43, co_row_cntr, co_col_cntr, co_cntr16, co_cntr_reg4, co_cntr13, cntr43_res,
-                 rst, rst_acc, rst_res_reg, mem_en, cntr16_img_en, cntr_filters_en, cntr4_filter_en, wr_file, adr_sel, mem_offset_sel, filter_wr_en,
-                 img_wr_en, row_cntr_en, col_cntr_en, cntr43_en, cntr16_en, img_slice_en, acc_en, cntr_reg4_en, res_buffer_en,
-                 cntr13_en, inc_en, inc_ld, done);
+module controller(clk, start,rst, rst_acc, rst_res_reg, mem_en, wr_file,
+            adr_sel, mem_offset_sel, filter_wr_en, img_wr_en, img_slice_en,
+            acc_en, res_buffer_en, inc_en, inc_ld, countr16_img, countr_filters,
+            countr4_filter, row_countr, col_countr, countr43, countr16, countr_reg4, done);
 
     input clk, start;
-    output reg rst, mem_en, cntr16_img_en, cntr_filters_en, cntr4_filter_en, adr_sel, filter_wr_en, wr_file;
-    output reg img_wr_en, row_cntr_en, col_cntr_en, cntr43_en, cntr16_en, img_slice_en, acc_en, cntr_reg4_en, res_buffer_en;
+    output reg rst, mem_en, adr_sel, filter_wr_en, wr_file;
+    output reg img_wr_en, col_cntr_en, img_slice_en, acc_en, res_buffer_en;
     output reg cntr13_en, inc_en, inc_ld, done, rst_acc, rst_res_reg;
     output reg[1:0] mem_offset_sel;
-    input co_cntr16_img, co_cntr4_filter, co_cntr_filters, co_cntr43, co_row_cntr, co_col_cntr, co_cntr16, co_cntr_reg4, co_cntr13;
-    input [7:0] cntr43_res;
-
+    output reg [7:0] countr16_img, countr4_filter, countr43, countr_filters, row_countr, col_countr, countr_reg4, countr16;
+    
+    reg [7:0] cntr16_img = 8'b0;
+    reg [7:0] cntr4_filter = 8'b0;
+    reg [7:0] cntr43 = 8'b0;
+    reg [7:0] cntr_filters = 8'b0;
+    reg [7:0] row_cntr = 8'b0;
+    reg [7:0] col_cntr = 8'b0;
+    reg [7:0] cntr16 = 8'b0;
+    reg [7:0] cntr_reg4 = 8'b0;
+    reg [7:0] cntr13 = 8'b0;
     reg[3:0] ns =`IDLE;
     reg[3:0] ps =`IDLE;
 
-    always @(start, co_cntr16_img, co_cntr4_filter, co_row_cntr,
-             co_cntr16, cntr43_res, co_cntr_reg4, co_cntr43, co_cntr13, ps) begin 
+    always @(start, cntr16_img, cntr4_filter, row_cntr,
+             cntr16, cntr43, cntr_reg4, co_cntr13, ps) begin 
         case(ps)
             `IDLE:   ns <= ~start ? `IDLE : `INIT;
             `INIT:   ns <= start ? `INIT : `LD_FILTER;
-            `LD_FILTER:   ns <= ~co_cntr4_filter ? `LD_FILTER : `NEXT_FILTER;
-            `NEXT_FILTER:   ns <= ~co_cntr_filters ? `LD_FILTER : `LD_SLICE;
-            `LD_SLICE:   ns <= ~co_cntr16_img ? `LD_SLICE : `LD_BUFFER;
-            `LD_BUFFER:   ns <= ~co_row_cntr ? `LD_BUFFER : `MAC;
-            `MAC:   ns <= ~co_cntr16 ? `MAC : `LD_RESULT;
-            `LD_RESULT :  ns <= (cntr43_res == 42 || co_cntr_reg4) ? `WRITE_MEM : `UPDATE;
-            `WRITE_MEM : ns <= co_cntr43 ? `DONE : `UPDATE;
-            `UPDATE : ns <= ~co_cntr13 ? `LD_BUFFER : `INC_OFFSET;
+            `LD_FILTER:   ns <= ~(cntr4_filter == 8'd3) ? `LD_FILTER : `NEXT_FILTER;
+            `NEXT_FILTER:   ns <= ~(cntr_filters == 8'd3) ? `LD_FILTER : `LD_SLICE;
+            `LD_SLICE:   ns <= ~(cntr16_img == 8'd15) ? `LD_SLICE : `LD_BUFFER;
+            `LD_BUFFER:   ns <= ~(row_cntr == 8'd3) ? `LD_BUFFER : `MAC;
+            `MAC:   ns <= ~(cntr16 == 8'd15) ? `MAC : `LD_RESULT;
+            `LD_RESULT :  ns <= (cntr43 == 8'd42|| cntr_reg4 == 8'd3) ? `WRITE_MEM : `UPDATE;
+            `WRITE_MEM : ns <= (cntr43 == 8'd42) ? `DONE : `UPDATE;
+            `UPDATE : ns <= ~(cntr13 == 8'd12) ? `LD_BUFFER : `INC_OFFSET;
             `INC_OFFSET : ns <= `LD_SLICE; 
             `DONE: ns <= `IDLE;
         endcase
     end
 
     always @(ps) begin
-        {rst, mem_en, cntr16_img_en, cntr_filters_en, cntr4_filter_en, adr_sel, filter_wr_en,
-        img_wr_en, row_cntr_en, col_cntr_en, cntr43_en, cntr16_en, img_slice_en, acc_en,
-        cntr_reg4_en, res_buffer_en, cntr13_en, inc_en, inc_ld, done, mem_offset_sel, wr_file, rst_acc, rst_res_reg} = 0;
+        {rst, mem_en, adr_sel, filter_wr_en,
+        img_wr_en, img_slice_en, acc_en,
+        res_buffer_en, cntr13_en, inc_en, inc_ld, done, mem_offset_sel, wr_file, rst_acc, rst_res_reg} = 0;
         case(ps)
             `INIT:begin  rst = 1; inc_ld = 1;   end
-            `LD_FILTER: begin cntr4_filter_en = 1; adr_sel = 1; filter_wr_en = 1; mem_offset_sel = 0; end
-            `LD_SLICE : begin  cntr16_img_en = 1; img_wr_en = 1; mem_offset_sel = 1;  end
-            `NEXT_FILTER: begin cntr_filters_en = 1; end
-            `LD_BUFFER: begin row_cntr_en = 1; img_slice_en = 1; end
-            `MAC: begin acc_en = 1; cntr16_en = 1; end
-            `LD_RESULT: begin cntr_reg4_en = 1; res_buffer_en = 1; rst_acc = 1;end
-            `WRITE_MEM: begin mem_en = 1; cntr43_en = 1; mem_offset_sel = 2; rst_res_reg = 1; end
-            `UPDATE: begin col_cntr_en = 1; cntr13_en = 1; end
-            `INC_OFFSET: begin inc_en = 1; end
+            `LD_FILTER: begin  adr_sel = 1; filter_wr_en = 1; mem_offset_sel = 0; end
+            `LD_SLICE : begin   img_wr_en = 1; mem_offset_sel = 1;  end
+            `NEXT_FILTER: begin  cntr4_filter = 8'b0; end
+            `LD_BUFFER: begin  img_slice_en = 1; end
+            `MAC: begin acc_en = 1; end
+            `LD_RESULT: begin res_buffer_en = 1; rst_acc = 1;end
+            `WRITE_MEM: begin mem_en = 1;  mem_offset_sel = 2; rst_res_reg = 1; end
+            `UPDATE: begin end
+            `INC_OFFSET: begin inc_en = 1; col_cntr = 8'b0; end
             `DONE: begin done = 1; wr_file = 1; end
+        endcase
+    end
+
+    always @(posedge clk) begin
+        case(ps)
+            `INIT:begin end
+            `LD_FILTER: begin cntr4_filter = cntr4_filter+ 1; end
+            `LD_SLICE : begin  cntr16_img = cntr16_img + 1; cntr_filters = 8'b0; end
+            `NEXT_FILTER: begin cntr_filters = cntr_filters + 1; end
+            `LD_BUFFER: begin row_cntr = row_cntr + 1; cntr16_img = 8'b0; end
+            `MAC: begin cntr16 = cntr16 + 1; row_cntr = 8'b0; end
+            `LD_RESULT: begin cntr_reg4 = cntr_reg4 + 1; cntr16 = 8'b0; end
+            `WRITE_MEM: begin cntr43 = cntr43 + 1; cntr_reg4 = 8'd0; end
+            `UPDATE: begin col_cntr = col_cntr + 1; cntr13 = cntr13 + 1; end
+            `INC_OFFSET: begin cntr13 = 8'b0; end
+            `DONE: begin cntr43 = 8'd0; end
         endcase
     end
 
@@ -71,6 +94,11 @@ module controller(clk, start, co_cntr16_img, co_cntr4_filter, co_cntr_filters,
          ps <= ns;
      end
 
-
-
+    assign countr16_img = cntr16_img;
+    assign countr_filters = cntr_filters;
+    assign countr43 = cntr43;
+    assign row_countr = row_cntr;
+    assign col_countr = col_cntr;
+    assign countr16 = cntr16;
+    assign countr_reg4 = cntr_reg4;
 endmodule
--- CA4/trunk/src/hdl/convolution.sv
+++ CA4/trunk/src/hdl/convolution.sv
@@ -4,22 +4,21 @@ module convolution(clk, start, x, y, z, done);
     input [7:0] x, y, z;
     output done;
 
-    wire rst, rst_acc, mem_en, cntr16_img_en, cntr_filters_en, cntr4_filter_en, adr_sel, filter_wr_en, rst_res_reg;
-    wire img_wr_en, row_cntr_en, col_cntr_en, cntr43_en, cntr16_en, img_slice_en, acc_en, cntr_reg4_en, res_buffer_en;
-    wire cntr13_en, inc_en, inc_ld, wr_file;
+    wire rst, rst_acc, mem_en, cntr16_img_en, adr_sel, filter_wr_en, rst_res_reg;
+    wire img_wr_en, img_slice_en, acc_en, res_buffer_en;
+    wire inc_en, inc_ld, wr_file;
     wire [1:0] mem_offset_sel;
-    wire co_cntr16_img, co_cntr4_filter, co_cntr_filters, co_cntr43, co_row_cntr, co_col_cntr, co_cntr16, co_cntr_reg4, co_cntr13;
-    wire [7:0] cntr43_res;
+    wire [7:0] countr16_img, countr4_filter, countr43, countr_filters, row_countr, col_countr, countr_reg4, countr16;
 
-    datapath #(NUM_FILTERS) dp(clk, rst, x, y, z, rst_acc, rst_res_reg, mem_en, cntr16_img_en, cntr_filters_en, cntr4_filter_en, filter_wr_en, img_wr_en, cntr43_en, row_cntr_en,
-          col_cntr_en, cntr16_en, img_slice_en, acc_en, cntr_reg4_en, res_buffer_en, cntr13_en,
-          inc_en, inc_ld, wr_file, adr_sel, mem_offset_sel, co_cntr16_img,
-          co_cntr4_filter, co_cntr_filters, co_cntr43, co_row_cntr, co_col_cntr, co_cntr16, co_cntr_reg4, co_cntr13, cntr43_res);
+    datapath #(NUM_FILTERS) dp(clk, rst, x, y, z, rst_acc, rst_res_reg, mem_en,
+        filter_wr_en, img_wr_en,  img_slice_en, acc_en, res_buffer_en,
+        inc_en, inc_ld, wr_file, adr_sel, mem_offset_sel, countr16_img,
+        countr4_filter, countr_filters, countr43, countr16_img,
+        row_countr, col_countr, countr16, countr_reg4);
 
-    controller cu(clk, start, co_cntr16_img, co_cntr4_filter, co_cntr_filters, co_cntr43, co_row_cntr, co_col_cntr,
-                 co_cntr16, co_cntr_reg4, co_cntr13, cntr43_res,
-                 rst, rst_acc, rst_res_reg, mem_en, cntr16_img_en, cntr_filters_en, cntr4_filter_en, wr_file, adr_sel, mem_offset_sel, filter_wr_en,
-                 img_wr_en, row_cntr_en, col_cntr_en, cntr43_en, cntr16_en, img_slice_en, acc_en, cntr_reg4_en, res_buffer_en,
-                 cntr13_en, inc_en, inc_ld, done);
+    controller cu(clk, start,rst, rst_acc, rst_res_reg, mem_en, wr_file,
+            adr_sel, mem_offset_sel, filter_wr_en, img_wr_en, img_slice_en,
+            acc_en, res_buffer_en, inc_en, inc_ld, countr16_img, countr_filters,
+            countr4_filter, row_countr, col_countr, countr43, countr16, countr_reg4, done);
 
 endmodule
