7f599c81920cd082912426a2c3d2ca060babf74c
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-01-12 16:57:15 +0330

    add layer2

--- CA4/trunk/src/hdl/conv_cu.sv
+++ CA4/trunk/src/hdl/conv_cu.sv
@@ -1,8 +1,10 @@
 `define IDLE 4'b0000
 `define INIT 4'b0001
-`define READ_MEM 4'b0010
+`define READ_MEM_L1 4'b0010
 `define PE_L1 4'b0011
 `define LD_RES_L1 4'b0101
+`define READ_MEM_L2 4'b0110
+`define PE_L2 4'b0111
 `define DONE 4'b0100
 
 module conv_cu(clk, start, done_mem_l1, done_pe_l1, done_mem_l2, done_pe_l2,
@@ -28,10 +30,12 @@ module conv_cu(clk, start, done_mem_l1, done_pe_l1, done_mem_l2, done_pe_l2,
     always @(ps, start, done_mem_l1, done_pe_l1, done_mem_l2, done_pe_l2) begin 
         case(ps)
             `IDLE:   ns <= ~start ? `IDLE : `INIT;
-            `INIT:   ns <= start ? `INIT : `READ_MEM;
-            `READ_MEM: ns <= done_mem_l1 ? `PE_L1 : `READ_MEM;
+            `INIT:   ns <= start ? `INIT : `READ_MEM_L1;
+            `READ_MEM_L1: ns <= done_mem_l1 ? `PE_L1 : `READ_MEM_L1;
             `PE_L1: ns <= done_pe_l1 ? `LD_RES_L1 : `PE_L1;
-            `LD_RES_L1: ns <= `DONE;
+            `LD_RES_L1: ns <= `READ_MEM_L2;
+            `READ_MEM_L2: ns <= done_mem_l2 ? `PE_L2 : `READ_MEM_L2; 
+            `PE_L2: ns <= done_pe_l2 ? `DONE : `PE_L2;
             `DONE: ns <= `IDLE; 
         endcase
     end
@@ -40,9 +44,11 @@ module conv_cu(clk, start, done_mem_l1, done_pe_l1, done_mem_l2, done_pe_l2,
         {wrmem_en_l2, start_mem_l1, start_pe_l1, start_mem_l2, start_pe_l2, done} = 0;
         case(ps)
             `INIT:begin  end
-            `READ_MEM : begin start_mem_l1 = 1; end
+            `READ_MEM_L1 : begin start_mem_l1 = 1; end
             `PE_L1 : begin start_pe_l1 = 1; end
             `LD_RES_L1 : begin wrmem_en_l2 = 1; end
+            `READ_MEM_L2 : begin start_mem_l2 = 1; end
+            `PE_L2 : begin start_pe_l2 = 1; end
             `DONE: begin done = 1; end
         endcase
     end
