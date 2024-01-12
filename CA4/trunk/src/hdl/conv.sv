2c54bc94e0949884206eb7d5d94298bd4d757ecd
elahekhodaverdi <102658681+elahekhodaverdi@users.noreply.github.com>
2024-01-12 16:36:03 +0330

    fix clk

--- CA4/trunk/src/hdl/conv.sv
+++ CA4/trunk/src/hdl/conv.sv
@@ -6,7 +6,7 @@ module conv(clk, start, x, y, z, done);
     conv_cu cu(clk, start, done_mem_l1, done_pe_l1, done_mem_l2, done_pe_l2,
           wrmem_en_l2, start_mem_l1, start_pe_l1, start_mem_l2, start_pe_l2, done);
 
-    conv_dp dp(cclk, start_mem_l1, start_pe_l1, start_mem_l2, start_pe_l2, wrmem_en_l2,
+    conv_dp dp(clk, start_mem_l1, start_pe_l1, start_mem_l2, start_pe_l2, wrmem_en_l2,
           x, y, z, done_mem_l1, done_pe_l1, done_mem_l2, done_pe_l2);
 
 
